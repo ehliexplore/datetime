7ae8ccf35ebe50b5b4d5cfc19f03ca9b4f967f8f
ehliexplore <lgspotifyemail@gmail.com>
2023-03-30 10:09:17 -0300

    greetings
    Adding "good morning", "good afternoon" and "good night"

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hour_now = data.getHours()
+    var hour_now = data.getHours() 
     var minutes_now = data.getMinutes()
     var day_now = data.getDate()
     var month_now = data.getMonth()
@@ -12,14 +12,17 @@ function carregar() {
     msg.innerHTML += `<p>${day_now}/${month_now}/${year_now}</p>`
     if (hour_now >= 0 && hour_now < 12) {
         // good morning
+        msg.innerHTML += `<p>Good morning</p>`
         img.src = 'morning.png'
         document.body.style.background = '#fee3b4'
     } else if (hour_now >= 12 && hour_now < 18) {
         // good afternoon
+        msg.innerHTML += `<p>Good afternoon</p>`
         document.body.style.background = '#4fa777'
         img.src = 'afternoon.png'
     } else {
         // good night
+        msg.innerHTML += `<p>Good night</p>`
         img.src = 'night.png'
         document.body.style.background = '#001834'
     }
